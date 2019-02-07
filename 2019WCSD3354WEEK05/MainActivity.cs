fb40d65eca1d3fd692d0d57600583ee661ae811c
visualstudio <visualstudio@230-PC34>
2019-02-07 13:56:10 -0500

    commit at 1.56

--- 2019WCSD3354WEEK05/MainActivity.cs
+++ 2019WCSD3354WEEK05/MainActivity.cs
@@ -1,53 +1,31 @@
 ﻿using System;
-using Android.App;
-using Android.OS;
-using Android.Runtime;
-using Android.Support.Design.Widget;
-using Android.Support.V7.App;
-using Android.Views;
-using Android.Widget;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace _2019WCSD3354WEEK05
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    class Program
     {
-
-        protected override void OnCreate(Bundle savedInstanceState)
+        static void Main(string[] args)
         {
-            base.OnCreate(savedInstanceState);
-            SetContentView(Resource.Layout.activity_main);
-
-            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
-            SetSupportActionBar(toolbar);
-
-            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
-            fab.Click += FabOnClick;
+            var a = new testques2();
+            a.Play();
         }
+    }
 
-        public override bool OnCreateOptionsMenu(IMenu menu)
+    class testques2
+    {
+        public void Play()
         {
-            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
-            return true;
-        }
+            //write a for loop
 
-        public override bool OnOptionsItemSelected(IMenuItem item)
-        {
-            int id = item.ItemId;
-            if (id == Resource.Id.action_settings)
+            for (; true;)
             {
-                return true;
+                Console.WriteLine("you are fool");
             }
 
-            return base.OnOptionsItemSelected(item);
         }
-
-        private void FabOnClick(object sender, EventArgs eventArgs)
-        {
-            View view = (View) sender;
-            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
-                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
-        }
-	}
+    }
 }
-
